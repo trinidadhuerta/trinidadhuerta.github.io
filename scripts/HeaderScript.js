fbfade8e747fe9d6322190b5f6c68100b6fb0d5a
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2020-02-11 13:40:36 -0600

    Styled Header Animation

--- scripts/HeaderScript.js
+++ scripts/HeaderScript.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+	
 	$('#hamburger').click(function(){
 		$(this).toggleClass('open');
 		$('nav').toggleClass('open');
+	});
+
+	$(window).scroll(function(){
+		var scrollPos = 200;
+
+		if ($(document).scrollTop() >= scrollPos){
+			$('.header-inner-wrapper').addClass('active');
+		}
+		else if ($(document).scrollTop() < scrollPos) {
+			$('.header-inner-wrapper').removeClass('active');
+		}
 	});
 });
