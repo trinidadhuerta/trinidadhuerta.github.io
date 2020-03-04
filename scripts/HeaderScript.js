0103c560322f6b4e3ea32472ed26b2afc3d55d79
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2020-03-04 14:37:24 -0600

    Update HeaderScript.js

--- scripts/HeaderScript.js
+++ scripts/HeaderScript.js
@@ -1,25 +1,12 @@
-window.onbeforeunload = function () {
-	window.scrollTo(0, 0);
-}
-
-
 $(document).ready(function(){
-	
-	$('#hamburger').click(function(){
-		$(this).toggleClass('open');
-		$('nav').toggleClass('open');
-	});
-
 	$(window).scroll(function(){
 		var scrollPos = 60;
 
 		if ($(document).scrollTop() >= scrollPos){
-			$('.header-inner-wrapper').addClass('active');
-			$('.landing-area-overlay').addClass('active');
+			$('#portfolio-header').addClass('active');
 		}
 		else if ($(document).scrollTop() < scrollPos) {
-			$('.header-inner-wrapper').removeClass('active');
-			$('.landing-area-overlay').removeClass('active');
+			$('#portfolio-header').removeClass('active');
 		}
 	});
 });
